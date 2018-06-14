85839aa74b0aa48edb66fe4b2eb2ec21113facde
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-06-14 22:37:43 +0100

    - add recursive function to find users before displaying to map - display new users to map
    - store users array in component

--- app/services/maps.js
+++ app/services/maps.js
@@ -4,24 +4,16 @@ const google = window.google;
 
 export default Service.extend({
 
-  init() {
-    this._super(...arguments);
-  },
-
-  createMapElement() {
+  createMapElement(locations) {
     const element = document.querySelector('#map');
     const rangeRadius = 500
     const center = { lat: 6.436914, lng: 3.451432 };
-    const center2 = { lat: 6.4326345, lng: 3.4489972 }
-    const center3 = { lat: 6.4357171, lng: 3.4492876 }
     let map = new google.maps.Map(element, { zoom: 16, center });
     // The marker, positioned at center
-    // new google.maps.Marker({ position: center, map: map });
-    // new google.maps.Marker({ position: center2, map: map, icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png' });
-    // new google.maps.Marker({ position: center3, map: map, icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png' });
     this.addMarker(center, map)
-    this.addMarker(center2, map, true)
-    this.addMarker(center3, map, true)
+    locations.forEach(location => {
+      this.addMarker(location, map, true)
+    })
 
     new google.maps.Circle({
       strokeColor: '#FF0000',
@@ -34,7 +26,7 @@ export default Service.extend({
       radius: rangeRadius
     });
 
-    this.locationDistance(center, center3)
+    // this.locationDistance(center, center3)
 
   },
 
@@ -44,8 +36,14 @@ export default Service.extend({
     } else {
       icon = ""
     }
-    new google.maps.Marker({ position: center, map: map, icon });
+    let parsedCenter = {
+      lat: parseFloat(center.lat),
+      lng: parseFloat(center.lng)
+    }
+    new google.maps.Marker({ position: parsedCenter, map: map, icon });
   },
+
+  addUsers() { },
 
   locationDistance(center1, center2) {
     const point1 = new google.maps.LatLng(center1.lat, center1.lng);
