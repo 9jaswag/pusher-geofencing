fce2b2b40349feb01828d910e8cd60db6ea030e1
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-06-14 14:44:02 +0100

    - add method for checking diff btw two distances

--- app/components/display-maps.js
+++ app/components/display-maps.js
@@ -3,9 +3,6 @@ import { inject as service } from '@ember/service';
 
 export default Component.extend({
   maps: service('maps'),
-  init() {
-    this._super(...arguments)
-  },
   didInsertElement() {
     this._super(...arguments);
     this.get('maps').createMapElement();
--- app/services/maps.js
+++ app/services/maps.js
@@ -10,10 +10,18 @@ export default Service.extend({
 
   createMapElement() {
     const element = document.querySelector('#map');
+    const rangeRadius = 500
     const center = { lat: 6.436914, lng: 3.451432 };
+    const center2 = { lat: 6.4326345, lng: 3.4489972 }
+    const center3 = { lat: 6.4357171, lng: 3.4492876 }
     let map = new google.maps.Map(element, { zoom: 16, center });
     // The marker, positioned at center
-    new google.maps.Marker({ position: center, map: map });
+    // new google.maps.Marker({ position: center, map: map });
+    // new google.maps.Marker({ position: center2, map: map, icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png' });
+    // new google.maps.Marker({ position: center3, map: map, icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png' });
+    this.addMarker(center, map)
+    this.addMarker(center2, map, true)
+    this.addMarker(center3, map, true)
 
     new google.maps.Circle({
       strokeColor: '#FF0000',
@@ -23,8 +31,26 @@ export default Service.extend({
       fillOpacity: 0.1,
       map: map,
       center: center,
-      radius: 500
+      radius: rangeRadius
     });
+
+    this.locationDistance(center, center3)
+
+  },
+
+  addMarker(center, map, icon = false) {
+    if (icon) {
+      icon = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+    }
+    new google.maps.Marker({ position: center, map: map, icon });
+  },
+
+  locationDistance(center1, center2) {
+    const point1 = new google.maps.LatLng(center1.lat, center1.lng);
+    const point2 = new google.maps.LatLng(center2.lat, center2.lng);
+
+    const distance = google.maps.geometry.spherical.computeDistanceBetween(point1, point2);
+    console.log(distance)
   }
 
 });
