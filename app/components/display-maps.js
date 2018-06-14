7686f0502203530cb18108e0d17cd0e209728f25
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-06-14 13:05:59 +0100

    - add service logic - add component logic

--- app/components/display-maps.js
+++ app/components/display-maps.js
@@ -1,4 +1,13 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+  maps: service('maps'),
+  init() {
+    this._super(...arguments)
+  },
+  didInsertElement() {
+    this._super(...arguments);
+    this.get('maps').createMapElement();
+  }
 });
--- app/services/maps.js
+++ app/services/maps.js
@@ -1,4 +1,30 @@
 import Service from '@ember/service';
 
+const google = window.google;
+
 export default Service.extend({
+
+  init() {
+    this._super(...arguments);
+  },
+
+  createMapElement() {
+    const element = document.querySelector('#map');
+    const center = { lat: 6.436914, lng: 3.451432 };
+    let map = new google.maps.Map(element, { zoom: 16, center });
+    // The marker, positioned at center
+    new google.maps.Marker({ position: center, map: map });
+
+    new google.maps.Circle({
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.2,
+      strokeWeight: 1,
+      fillColor: '#FF0000',
+      fillOpacity: 0.1,
+      map: map,
+      center: center,
+      radius: 500
+    });
+  }
+
 });
